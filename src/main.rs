728ebf930a8efb995dd43702398bc26d8e0a8c9f
sandhilt <6170125+sandhilt@users.noreply.github.com>
2024-08-30 11:25:36 -0300

    feat: remove web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
+use std::error::Error;
+
+use builder_cartesi::http_service;
+
+#[actix_web::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!");
+    match http_service::create_server() {
+        Ok(server) => {
+            server.await?;
+            Ok(())
+        }
+        Err(err) => {
+            eprintln!("Error creating server: {}", err);
+            Err(err)
+        }
+    }
 }
